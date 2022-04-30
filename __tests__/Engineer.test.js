5726f2ec8eeaf70a8c4fed362c96681dbb5561a3
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-04-30 12:54:49 -0400

    add testing and made sure it passed engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,8 +1,11 @@
 
+const { exportAllDeclaration } = require('@babel/types');
 const Engineer = require('../lib/Engineer')
 
 test('creates engineer github username and github', () => {
-    const engineer = new Engineer('Nicole', 4, 'testing@test.com', 'NicoleSharrock');
+    const engineer = new Engineer('Tara', 3, 'testing@test.com', 'tarahayes');
 
-
+    expect(engineer.gitHub).toBe('tarahayes');
+    expect(engineer.getGithub()).toBe('tarahayes');
+    expect(engineer.getRole()), toBe('Engineer');
 })
