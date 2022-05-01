b34601d1248698e9f4b26b59e2719cf32e2de9fe
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-05-01 18:34:01 -0400

    fixed err in manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,6 +4,5 @@ test('create an office number and return of Manager', () => {
     const manager = new Manager('Sharrock', 1, 'testing@test.com', 100);
 
     expect(manager.officeNumber).toBe(100);
-    expect(manager.getOfficeNumber()).toBe(100);
     expect(manager.getRole()).toBe('Manager');
 })
